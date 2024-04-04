c30eeec0c3e998ef38579da95ad8044e574fcf91
devCainLipsey <129211827+devCainLipsey@users.noreply.github.com>
2024-04-04 00:02:07 -0500

    Updates
    Removed NavBar from 404 Error Page.
    
    Changed button on 404 Error Page.
    
    Changed tab meta data title and description.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Link from "next/link";
-import NavBar from "@/components/NavBar";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const NotFound: React.FC = () => {
   return (
     <>
-      <NavBar />
       <div className="bg-gradient-to-r from-slate-950 via-purple-600 to-slate-950 flex flex-col items-center justify-center min-h-screen">
         <div className="bg-black rounded-3xl py-10 px-10">
           <h1 className="text-4xl font-bold text-white mb-4">
@@ -17,9 +16,15 @@ const NotFound: React.FC = () => {
           <div className="flex justify-center">
             <Link href="/">
               <div className="w-64 relative inline-flex group mt-12">
-                <div className="absolute transition-all duration-100 opacity-70 -inset-px bg-gradient-to-r from-[#8554c7] via-[#cf0cbc] to-[#8554c7] rounded-3xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
-                <button className="w-64 relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-100 bg-black font-pj rounded-3xl focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[#cf0cbc]">
-                  Go Home
+                <div className="absolute transition-all duration-100 opacity-50 -inset-px bg-gradient-to-r from-[#cf0cbc] via-[#8554c7] to-[#cf0cbc] rounded-3xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+                <button className="w-64 relative inline-flex items-center justify-center px-8 py-4 text-xl font-bold text-white transition-all duration-100 bg-black font-pj rounded-3xl focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-[#cf0cbc] group-hover:pl-6">
+                  <span className="ml-5 group-hover:translate-x-[-1rem] transition-all duration-200 text-center">
+                    Go Home
+                  </span>
+                  <FaLongArrowAltRight
+                    className="-ml-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+                    size={20}
+                  />
                 </button>
               </div>
             </Link>
