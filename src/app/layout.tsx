41628e610063a4394fc59bfbd7ff6c2523f78353
devCainLipsey <129211827+devCainLipsey@users.noreply.github.com>
2024-06-03 03:09:22 -0500

    Update layout.tsx
    Updated layout.tsx for font domain

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto_Serif } from "next/font/google";
+import Head from "next/head";
 import "./globals.css";
 import ReduxProvider from "@/redux/features/provider";
 
@@ -17,6 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="preload"
+          href="https://fonts.gstatic.com/s/robotoserif/v13/R70mjywflP6FLr3gZx7K8UyuXDs9zVwDmXCb8lxYgmuii32UGoVldX6UgfjL4-3sMM_kB_qXSEX5JCh01uHN-poF.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body className={roboto_serif.className}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
